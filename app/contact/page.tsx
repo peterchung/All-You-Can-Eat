5472213b70e2611299e97217f40131e0b685bf33
peter chung <peter.chung.716@gmail.com>
2024-03-22 14:31:22 -0400

    Add form registers

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,12 @@
 'use client';
 
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { handleSubmit } from '@/utils/actions';
+
 const Contact = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
     <div className='contact-form-wrapper mt-40 mb-20 flex flex-col items-center'>
       <form>
@@ -13,11 +19,19 @@ const Contact = () => {
           </legend>
           <div className='first-name-wrapper flex flex-col'>
             <label>First Name</label>
-            <input type='text' className='first-name p-2.5 text-black' />
+            <input
+              {...register('firstName', { required: true, minLength: 2 })}
+              type='text'
+              className='first-name p-2.5 text-black'
+            />
           </div>
           <div className='last-name-wrapper flex flex-col'>
             <label>Last Name</label>
-            <input type='text' className='last-name p-2.5 text-black' />
+            <input
+              {...register('lastName', { required: true, minLength: 2 })}
+              type='text'
+              className='last-name p-2.5 text-black'
+            />
           </div>
         </fieldset>
         <fieldset className='item-field flex gap-x-2.5 mb-4'>
@@ -27,7 +41,11 @@ const Contact = () => {
               <span className='text-lg'>(required)</span>
             </label>
           </legend>
-          <input type='email' className='email p-2.5 w-full text-black' />
+          <input
+            {...register('email', { required: true, minLength: 4 })}
+            type='email'
+            className='email p-2.5 w-full text-black'
+          />
         </fieldset>
         <fieldset className='item-field flex gap-x-2.5 mb-4'>
           <legend className='pb-1'>
@@ -36,7 +54,11 @@ const Contact = () => {
               <span className='text-lg'>(required)</span>
             </label>
           </legend>
-          <input type='text' className='subject p-2.5 w-full text-black' />
+          <input
+            {...register('subject', { required: true, minLength: 3 })}
+            type='text'
+            className='subject p-2.5 w-full text-black'
+          />
         </fieldset>
         <fieldset className='item-field flex gap-x-2.5 mb-4'>
           <legend className='pb-1'>
@@ -45,12 +67,19 @@ const Contact = () => {
               <span className='text-lg'>(required)</span>
             </label>
           </legend>
-          <textarea className='message p-2.5 w-full text-black h-32'></textarea>
+          <textarea
+            {...register('message', { required: true, minLength: 4 })}
+            className='message p-2.5 w-full text-black h-32'
+          ></textarea>
         </fieldset>
-        <button className='bg-gray-200 font-bold text-black uppercase py-4 px-6 rounded hover:opacity-50'>
+        <button
+          type='submit'
+          className='bg-gray-200 font-bold text-black uppercase py-4 px-6 rounded hover:opacity-50'
+        >
           Submit
         </button>
       </form>
+      {/* <div>{JSON.stringify(state, null, 2)}</div> */}
     </div>
   );
 };
