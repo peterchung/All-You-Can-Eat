d7bf2ee887ab9a8a1a2e998f5a4cf83fa2b2d4c6
peter chung <peter.chung.716@gmail.com>
2024-04-02 10:05:00 -0400

    Create Sanity product image schema

---
+++ ayce-ecommerce/schemaTypes/images.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'images',
+  type: 'document',
+  title: 'Product Images',
+  fields: [
+    {
+      name: 'image1',
+      type: 'image',
+      title: 'First Image',
+    },
+    {
+      name: 'image2',
+      type: 'image',
+      title: 'Second Image',
+    },
+  ],
+}
--- ayce-ecommerce/schemaTypes/index.ts
+++ ayce-ecommerce/schemaTypes/index.ts
@@ -1,3 +1,4 @@
+import images from './images'
 import product from './product'
 
-export const schemaTypes = [product]
+export const schemaTypes = [product, images]
