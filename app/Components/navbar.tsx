251fe282af87076af52237dd16722f8f4fd89033
peter chung <peter.chung.716@gmail.com>
2024-04-02 12:03:31 -0400

    Clear comments from navbar component

--- app/Components/navbar.tsx
+++ app/Components/navbar.tsx
@@ -4,8 +4,6 @@ import Image from 'next/image';
 export default function Navbar() {
   return (
     <header>
-      {/* navbar */}
-
       <div className='fixed top-0 w-full z-50'>
         <nav className='flex justify-between items-center m-5'>
           <div className=''>
@@ -35,29 +33,6 @@ export default function Navbar() {
           </ul>
         </nav>
       </div>
-
-      {/* <div className='flex-grow'>
-        <div
-          className='absolute inset-0 bg-cover bg-center'
-          style={{
-            backgroundImage: 'url("/images/landing-image-temp.png")',
-          }}
-        ></div>
-      </div> */}
-
-      {/* podcast about one liner */}
-      {/* <div className='flex justify-center items-end mb-20 z-10'>
-        <div className='mx-auto max-w-6xl'>
-          <div className='font-bold text-white text-5xl my-10'>
-            <h1>TWO ASIANS EXPLORING ASIAN AMERICAN-ISH EXPERIENCES</h1>
-          </div>
-          <div>
-            <button className='bg-gray-200 hover:opacity-50 font-bold text-black py-4 px-6 rounded-full'>
-              LISTEN NOW
-            </button>
-          </div>
-        </div>
-      </div> */}
     </header>
   );
 }
