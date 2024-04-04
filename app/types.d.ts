a6fb96dd28345e8b39a35a5b12cdd4d607c98a4c
peter chung <peter.chung.716@gmail.com>
2024-04-04 10:59:28 -0400

    Create interface for single full product

--- app/types.d.ts
+++ app/types.d.ts
@@ -16,6 +16,15 @@ export interface simplifiedProduct {
   imageUrl: string;
 }
 
+export interface fullProduct {
+  _id: string;
+  price: number;
+  name: string;
+  slug: string;
+  images: any;
+  description: string;
+}
+
 // export interface FormResultError {
 //   emailSent: boolean;
 //   errors: ValidationErrors;
