7a46107812393eda090f3d068459a2c3afbf16b4
peter chung <peter.chung.716@gmail.com>
2024-04-04 11:14:15 -0400

    Update component names to Pascal case

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from './components/navbar';
-import Footer from './components/footer';
-import Providers from './components/providers';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Providers from './components/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
