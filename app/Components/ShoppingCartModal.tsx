c75434bca1a7d6a8775ac38d89f75a347052ea7b
peter chung <peter.chung.716@gmail.com>
2024-04-12 06:47:13 -0400

    Add subtotal, checkout button and continue shopping button. Add functionality to close cart modal when continue shopping is clicked.

--- app/Components/ShoppingCartModal.tsx
+++ app/Components/ShoppingCartModal.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import {
   Sheet,
   SheetContent,
@@ -16,6 +17,7 @@ export default function ShoppingCartModal() {
     handleCartClick,
     cartDetails,
     removeItem,
+    totalPrice,
   } = useShoppingCart();
 
   return (
@@ -71,6 +73,31 @@ export default function ShoppingCartModal() {
                 </>
               )}
             </ul>
+          </div>
+
+          <div className='border-t border-gray-200 px-4 py-6 sm:px-6'>
+            <div className='flex justify-between text-base font-medium text-gray-900'>
+              <p>Subtotal</p>
+              <p>${totalPrice}</p>
+            </div>
+            <p className='mt-0.5 text-sm text-gray-500'>
+              Shipping and taxes calculated at checkout
+            </p>
+
+            <div className='mt-6'>
+              <Button className='w-full'>Checkout</Button>
+            </div>
+            <div className='mt-6 flex justify-center text-center text-sm text-gray-500'>
+              <p>
+                OR{' '}
+                <button
+                  className='font-medium text-primary hover:text-primary/80'
+                  onClick={() => handleCartClick()}
+                >
+                  Continue Shopping
+                </button>
+              </p>
+            </div>
           </div>
         </div>
       </SheetContent>
