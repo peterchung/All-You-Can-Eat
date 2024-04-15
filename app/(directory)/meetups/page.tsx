9f7b4ca66f7e0e502e7fb3c7e172aaf890dc3a39
peter chung <peter.chung.716@gmail.com>
2024-04-15 17:56:58 -0400

    Configure tailwind.config to get marquee text scrolling effect and create text scroll with 'hello' in various asian languages

---
+++ app/(directory)/meetups/page.tsx
@@ -0,0 +1,39 @@
+export default function Meetups() {
+  return (
+    <div className='relative flex overflow-x-hidden'>
+      <div className='py-12 animate-marquee whitespace-nowrap uppercase'>
+        <span className='text-4xl mx-4'>hello</span>
+        <span className='text-4xl mx-4'>안녕</span>
+        <span className='text-4xl mx-4'>こんにちは</span>
+        <span className='text-4xl mx-4'>你好</span>
+        <span className='text-4xl mx-4'>Xin chào</span>
+        <span className='text-4xl mx-4'>สวัสดีo</span>
+        <span className='text-4xl mx-4'>नमस्ते</span>
+        <span className='text-4xl mx-4'>hello</span>
+        <span className='text-4xl mx-4'>안녕</span>
+        <span className='text-4xl mx-4'>こんにちは</span>
+        <span className='text-4xl mx-4'>你好</span>
+        <span className='text-4xl mx-4'>Xin chào</span>
+        <span className='text-4xl mx-4'>สวัสดีo</span>
+        <span className='text-4xl mx-4'>नमस्ते</span>
+      </div>
+
+      <div className='absolute top-0 py-12 animate-marquee2 whitespace-nowrap uppercase'>
+        <span className='text-4xl mx-4'>hello</span>
+        <span className='text-4xl mx-4'>안녕</span>
+        <span className='text-4xl mx-4'>こんにちは</span>
+        <span className='text-4xl mx-4'>你好</span>
+        <span className='text-4xl mx-4'>Xin chào</span>
+        <span className='text-4xl mx-4'>สวัสดีo</span>
+        <span className='text-4xl mx-4'>नमस्ते</span>
+        <span className='text-4xl mx-4'>hello</span>
+        <span className='text-4xl mx-4'>안녕</span>
+        <span className='text-4xl mx-4'>こんにちは</span>
+        <span className='text-4xl mx-4'>你好</span>
+        <span className='text-4xl mx-4'>Xin chào</span>
+        <span className='text-4xl mx-4'>สวัสดีo</span>
+        <span className='text-4xl mx-4'>नमस्ते</span>
+      </div>
+    </div>
+  );
+}
