c400c0ac0a10775bd7ed2cf4a110c3bd01fd54e2
peter chung <peter.chung.716@gmail.com>
2024-04-17 00:20:19 -0400

    Add background image to meetups page

---
+++ ayce-ecommerce/schemaTypes/meetupsImages.ts
@@ -0,0 +1,12 @@
+export default {
+  name: 'meetupsImages',
+  type: 'document',
+  title: 'Meetups Images',
+  fields: [
+    {
+      name: 'image1',
+      type: 'image',
+      title: 'First Image',
+    },
+  ],
+}
