76f31f81106ea37337e9506417f064dea30c80e1
peter chung <peter.chung.716@gmail.com>
2024-04-24 12:02:05 -0400

    Update background image to use nextjs Image component

--- app/Components/MarqueeText.tsx
+++ app/Components/MarqueeText.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { client, urlFor } from '../lib/sanity';
 
 const getData = async () => {
@@ -12,11 +13,17 @@ export default async function MarqueeText() {
   const data = await getData();
   //TODO: Add onClick to open new tab to join discord server
   return (
-    <div
-      className='h-screen bg-cover bg-center'
-      style={{ backgroundImage: `url(${urlFor(data.image1).url()})` }}
-    >
-      <div className='relative flex overflow-x-hidden'>
+    <div className='relative h-screen'>
+      <div className='absolute inset-0'>
+        <Image
+          src={urlFor(data.image1).url()}
+          alt='Landing Background'
+          fill={true}
+          style={{ objectFit: 'cover', objectPosition: 'center' }}
+          priority
+        />
+      </div>
+      <div className='relative flex overflow-x-hidden z-10'>
         <div className='py-7 animate-marquee whitespace-nowrap uppercase'>
           <span className='text-4xl mx-4'>hello</span>
           <span className='text-4xl mx-4'>안녕</span>
@@ -51,18 +58,18 @@ export default async function MarqueeText() {
           <span className='text-4xl mx-4'>नमस्ते</span>
         </div>
       </div>
-      <div className='w-11/12 h-5/6 mx-auto flex flex-col justify-start items-center'>
-        <h2 className=' uppercase py-4 text-2xl md:text-4xl font-semibold'>
+      <div className='w-11/12 h-5/6 mx-auto flex flex-col justify-start items-center z-10'>
+        <h2 className=' uppercase py-4 text-2xl md:text-4xl font-semibold z-10'>
           dates coming soon
         </h2>
-        <p className='w-3/5 text-outline font-semibold leading-relaxed text-sm md:text-lg'>
+        <p className='w-3/5 text-outline font-semibold leading-relaxed text-sm md:text-lg z-10'>
           Join our Discord Community to chat, share interests, play games, watch
           movies, and support each other's passions! Our community is meant for
           members to meet new friends, engage in discussion, and encourage one
           another in our endeavors. Grow with us as we support each other on our
           collective journey!
         </p>
-        <div className='py-4'>
+        <div className='py-4 z-10'>
           <button className='bg-gray-200 hover:opacity-50 font-bold text-black py-4 px-6 rounded-full uppercase'>
             join server
           </button>
