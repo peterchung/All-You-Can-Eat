114636c2975f388c189b6505aacc714375aef0da
peter chung <peter.chung.716@gmail.com>
2024-05-14 18:31:21 -0400

    Update stripe order success and cancel url to lead to success or error page

--- app/component/Providers.tsx
+++ app/component/Providers.tsx
@@ -4,14 +4,16 @@ import React from 'react';
 import { Toaster } from 'sonner';
 import { CartProvider } from 'use-shopping-cart';
 
+//TODO: Update success and cancel url to aycepod.com
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CartProvider
       mode='payment'
       cartMode='client-only'
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl='http://localhost:3000/order/success'
-      cancelUrl='http://localhost:3000/order/error'
+      successUrl='https://www.aycepod.com/order/success'
+      cancelUrl='https://www.aycepod.com/order/error'
       currency='USD'
       billingAddressCollection={true}
       shouldPersist={true}
